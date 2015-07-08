dca34b54bd809459e9270eb499d925b28e668169
Bruno Fabbri <fabbri07@gmail.com>
2015-07-08 19:13:51 -0300

    Added Average Score Calculation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,11 @@ class Review < ActiveRecord::Base
   belongs_to :place
 
   validates_presence_of [:score, :content]
+
+  after_save :calculate_average
+
+  def calculate_average
+    a = self.place.average_rating
+    self.place.update_attributes(total_average_rating: a)
+  end
 end
