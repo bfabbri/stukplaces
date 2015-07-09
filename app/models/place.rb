1ee584d0f85add2b6ae6945b68aab4005ba0ff97
Bruno Fabbri <fabbri07@gmail.com>
2015-07-08 22:27:48 -0300

    Added search to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,4 +10,13 @@ class Place < ActiveRecord::Base
   rescue ZeroDivisionError
     0
   end
+
+  def self.search(search)
+    if search
+      where(['name LIKE ? OR address LIKE ?', "#{search}", "#{search}"])
+    else
+      all
+    end
+  end
+
 end
